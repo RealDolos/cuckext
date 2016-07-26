2537e583cafcd9e490ca90e1f9e44c97df4d577f
RealDolos <dolos@cock.li>
2016-07-27 01:48:06 +0200

    Cucks 2

--- overlay.js
+++ overlay.js
@@ -1,6 +1,26 @@
 addEventListener("load", e => {
+    function list_tabs() {
+      let rv = [];
+      for (let tab of Array.from(gBrowser.tabs)) {
+        rv.push(tab.linkedBrowser.currentURI.spec);
+      }
+      return rv;
+    }
+
     let mi = document.querySelector("#cucks_menuitem");
     mi.addEventListener("click", e => {
-        alert("auxo!!!");
+        alert(list_tabs().join("\n"));
+        for (let tab of Array.from(gBrowser.tabs)) {
+            try {
+                let host = tab.linkedBrowser.currentURI.host;
+                console.log(host);
+                if (host === "www.mozilla.org") {
+                    gBrowser.removeTab(tab);
+                }
+            }
+            catch (ex) {
+                console.error(ex);
+            }
+        }
     });
 });
