3e0fa87686dcda911362325022efc8563e4bff11
RealDolos <dolos@cock.li>
2016-07-27 02:01:04 +0200

    Moar

--- overlay.js
+++ overlay.js
@@ -1,26 +1,53 @@
 addEventListener("load", e => {
-    function list_tabs() {
-      let rv = [];
-      for (let tab of Array.from(gBrowser.tabs)) {
-        rv.push(tab.linkedBrowser.currentURI.spec);
-      }
-      return rv;
-    }
+  function uriOf(tab) {
+    return tab.linkedBrowser.currentURI;
+  }
 
-    let mi = document.querySelector("#cucks_menuitem");
-    mi.addEventListener("click", e => {
-        alert(list_tabs().join("\n"));
-        for (let tab of Array.from(gBrowser.tabs)) {
-            try {
-                let host = tab.linkedBrowser.currentURI.host;
-                console.log(host);
-                if (host === "www.mozilla.org") {
-                    gBrowser.removeTab(tab);
-                }
-            }
-            catch (ex) {
-                console.error(ex);
-            }
+  function hostOf(tab) {
+    let host = uriOf(tab).host;
+    if (!host) {
+      throw Error("invalid host");
+    }
+    try {
+      return Services.eTLD.getBaseDomainFromHost(host);
+    }
+    catch (ex) {
+      return host;
+    }
+  }
+
+  let mi = document.querySelector("#cucks_menuitem");
+  let menu = document.querySelector("#tabContextMenu");
+
+  menu.addEventListener("popupshowing", e => {
+      let host = "cucks";
+      try {
+        host = hostOf(TabContextMenu.contextTab);
+      }
+      catch (ex) {
+        console.error(ex);
+      }
+      mi.setAttribute("label", `Close all ${host} tabs`);
+  }, true);
+
+  mi.addEventListener("click", e => {;
+      let host = null;
+      try {
+        host = hostOf(TabContextMenu.contextTab);
+      }
+      catch (ex) {
+        return;
+      }
+
+      for (let tab of Array.from(gBrowser.tabs)) {
+        try {
+          if (hostOf(tab) === host) {
+            gBrowser.removeTab(tab);
+          }
         }
-    });
+        catch (ex) {
+          console.error(ex);
+        }
+      }
+  });
 });
