db36a91086dece6d85c2cc35711dead2f711885c
RealDolos <dolos@cock.li>
2016-07-27 02:13:30 +0200

    Move it!

--- overlay.js
+++ overlay.js
@@ -16,38 +16,71 @@ addEventListener("load", e => {
     }
   }
 
-  let mi = document.querySelector("#cucks_menuitem");
+  let close = document.querySelector("#cucks_close");
+  let move = document.querySelector("#cucks_move");
   let menu = document.querySelector("#tabContextMenu");
 
   menu.addEventListener("popupshowing", e => {
-      let host = "cucks";
+    let host = "cucks";
+    try {
+      host = hostOf(TabContextMenu.contextTab);
+    }
+    catch (ex) {
+      console.error(ex);
+    }
+    close.setAttribute("label", `Close all ${host} tabs`);
+    move.setAttribute("label", `Move all ${host} tabs to a new window`);
+  }, true);
+
+  close.addEventListener("click", e => {;
+    let host = null;
+    try {
+      host = hostOf(TabContextMenu.contextTab);
+    }
+    catch (ex) {
+      return;
+    }
+
+    for (let tab of Array.from(gBrowser.tabs)) {
       try {
-        host = hostOf(TabContextMenu.contextTab);
+        if (hostOf(tab) === host) {
+          gBrowser.removeTab(tab);
+        }
       }
       catch (ex) {
         console.error(ex);
       }
-      mi.setAttribute("label", `Close all ${host} tabs`);
-  }, true);
+    }
+  });
 
-  mi.addEventListener("click", e => {;
-      let host = null;
+  move.addEventListener("click", e => {;
+    let host = null;
+    try {
+      host = hostOf(TabContextMenu.contextTab);
+    }
+    catch (ex) {
+      return;
+    }
+
+    let tomove = [];
+    for (let tab of Array.from(gBrowser.tabs)) {
       try {
-        host = hostOf(TabContextMenu.contextTab);
+        if (hostOf(tab) === host) {
+          tomove.push(tab);
+        }
       }
       catch (ex) {
-        return;
+        console.error(ex);
       }
-
-      for (let tab of Array.from(gBrowser.tabs)) {
-        try {
-          if (hostOf(tab) === host) {
-            gBrowser.removeTab(tab);
-          }
-        }
-        catch (ex) {
-          console.error(ex);
-        }
-      }
+    }
+    if (!tomove.length) {
+      return;
+    }
+    let anchor = tomove.shift();
+    tomove.reverse();
+    let newwin = gBrowser.replaceTabWithWindow(anchor);
+    newwin.addEventListener("load", e => {
+      tomove.forEach(t => newwin.gBrowser.adoptTab(t, 1, false));
+    });
   });
 });
